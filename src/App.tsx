f52887b01b381601266bfbf135b3779ec704fb69
vpagano10 <vpagano10@gmail.com>
2020-08-17 20:29:49 -0400

    generatecells function now calculates number of adjacent mines

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import "./styles/App.scss";
 // BUILD GAME BOARD
 const MAX_ROWS: number = 9;
 const MAX_COLS: number = 9;
+const NUM_MINES: number = 10;
 const generateCells = (): Cell[][] => {
   let cells: Cell[][] = [];
   for (let rows = 0; rows < MAX_ROWS; rows++) {
@@ -18,6 +19,90 @@ const generateCells = (): Cell[][] => {
       });
     }
   }
+
+  // PLACE MINES
+  let minesPlaced = 0;
+  while (minesPlaced < NUM_MINES) {
+    const randomRow = Math.floor(Math.random() * MAX_ROWS);
+    const randomCol = Math.floor(Math.random() * MAX_COLS);
+    const currentCell = cells[randomRow][randomCol];
+    if (currentCell.value !== CellValue.bomb) {
+      cells = cells.map((row, rowindex) =>
+        row.map((cell, colindex) => {
+          if (randomRow == rowindex && randomCol == colindex) {
+            return {
+              ...cell,
+              value: CellValue.bomb,
+            };
+          }
+          return cell;
+        })
+      );
+      minesPlaced++;
+    }
+  }
+
+  // CALCULATE NUMBERS IN CELLS
+  for (let rIndex = 0; rIndex < MAX_ROWS; rIndex++) {
+    for (let cIndex = 0; cIndex < MAX_COLS; cIndex++) {
+      const currentCell = cells[rIndex][cIndex];
+      if (currentCell.value == CellValue.bomb) {
+        continue;
+      }
+      let numBombs = 0;
+      const NW =
+        rIndex > 0 && cIndex > 0 ? cells[rIndex - 1][cIndex - 1] : null;
+      const N = rIndex > 0 ? cells[rIndex - 1][cIndex] : null;
+      const NE =
+        rIndex > 0 && cIndex < MAX_COLS - 1
+          ? cells[rIndex - 1][cIndex + 1]
+          : null;
+      const W = cIndex > 0 ? cells[rIndex][cIndex - 1] : null;
+      const E = cIndex < MAX_COLS - 1 ? cells[rIndex][cIndex + 1] : null;
+      const SW =
+        rIndex < MAX_ROWS - 1 && cIndex > 0
+          ? cells[rIndex + 1][cIndex - 1]
+          : null;
+      const S = rIndex < MAX_ROWS - 1 ? cells[rIndex + 1][cIndex] : null;
+      const SE =
+        rIndex < MAX_ROWS - 1 && cIndex < MAX_COLS - 1
+          ? cells[rIndex + 1][cIndex + 1]
+          : null;
+
+      if (NW && NW.value == CellValue.bomb) {
+        numBombs++;
+      }
+      if (N && N.value == CellValue.bomb) {
+        numBombs++;
+      }
+      if (NE && NE.value == CellValue.bomb) {
+        numBombs++;
+      }
+      if (W && W.value == CellValue.bomb) {
+        numBombs++;
+      }
+      if (E && E.value == CellValue.bomb) {
+        numBombs++;
+      }
+      if (SW && SW.value == CellValue.bomb) {
+        numBombs++;
+      }
+      if (S && S.value == CellValue.bomb) {
+        numBombs++;
+      }
+      if (SE && SE.value == CellValue.bomb) {
+        numBombs++;
+      }
+
+      if (numBombs > 0) {
+        cells[rIndex][cIndex] = {
+          ...currentCell,
+          value: numBombs,
+        };
+      }
+    }
+  }
+
   return cells;
 };
 
@@ -26,7 +111,15 @@ const App: React.FC = () => {
 
   const renderCells = (): React.ReactNode => {
     return cells.map((row, rowIndex) =>
-      row.map((cell, colIndex) => <Buttons key={`${rowIndex}-${colIndex}`} />)
+      row.map((cell, colIndex) => (
+        <Buttons
+          key={`${rowIndex}-${colIndex}`}
+          state={cell.state}
+          value={cell.value}
+          row={rowIndex}
+          col={colIndex}
+        />
+      ))
     );
   };
 
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,44 @@
 import React from "react";
 import "../styles/Button.scss";
+import { CellState, CellValue } from "../types";
 
-export const Buttons: React.FC = () => {
-  return <div className="button" />;
+interface ButtonsProps {
+  row: number;
+  col: number;
+  state: CellState;
+  value: CellValue;
+}
+
+export const Buttons: React.FC<ButtonsProps> = ({ row, col, state, value }) => {
+  const renderContent = (): React.ReactNode => {
+    if (state == CellState.visible) {
+      if (value == CellValue.bomb) {
+        return (
+          <span role="img" aria-label="bomb">
+            💣
+          </span>
+        );
+      } else if (value == CellValue.none) {
+        return null;
+      }
+      return value;
+    } else if (state == CellState.flagged) {
+      return (
+        <span role="img" aria-label="bomb">
+          🚩
+        </span>
+      );
+    }
+    return null;
+  };
+
+  return (
+    <div
+      className={`button ${
+        state == CellState.visible ? "visible" : ""
+      } value-${value}`}
+    >
+      {renderContent()}
+    </div>
+  );
 };
